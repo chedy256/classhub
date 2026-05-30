b9b5bfb2bc35f11a1eda2ed36349310714f31e47
chedy256 <chedyelhaj@gmail.com>
2026-05-30 01:58:21 +0100

    chore: updated dependencies and removed deprecated_member_use

--- lib/file_explorer/screens/main_screen.dart
+++ lib/file_explorer/screens/main_screen.dart
@@ -334,7 +334,7 @@ class _MainScreenState extends State<MainScreen>
 
   Future<void> _uploadFiles() async {
     _toggleFab();
-    final result = await FilePicker.pickFiles(allowMultiple: true);
+    final result = await FilePicker.pickFiles();
     if (result != null && result.files.isNotEmpty) {
       final paths = result.files
           .where((pf) => pf.path != null)
@@ -1241,7 +1241,7 @@ class _InsideFolderScreenState extends State<_InsideFolderScreen>
 
   Future<void> _uploadFiles() async {
     _toggleFab();
-    final result = await FilePicker.pickFiles(allowMultiple: true);
+    final result = await FilePicker.pickFiles();
     if (result != null && result.files.isNotEmpty) {
       final paths = result.files
           .where((pf) => pf.path != null)
